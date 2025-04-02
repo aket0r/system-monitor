cd22cf91f6bfb6f165d15ca0b8942ca251f5de2a
Aket0r <zerotwozefrankxx@gmail.com>
2025-04-02 19:39:15 +0200

    added required files

---
+++ src/index.js
@@ -0,0 +1,88 @@
+const { app, BrowserWindow, Tray, Menu } = require('electron');
+const path = require('path');
+
+let mainWindow = null;
+let tray = null;
+
+function createWindow() {
+  if (mainWindow) {
+    mainWindow.show();
+    mainWindow.focus();
+    return;
+  }
+
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    show: false,
+    icon: path.join(__dirname, 'assets/icons/icon.ico'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
+  });
+
+  mainWindow.maximize();
+
+  mainWindow.loadFile('src/main.html');
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('close', (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'assets/icons/icon.ico'));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show',
+      click: () => {
+        if (mainWindow) {
+          mainWindow.show();
+          mainWindow.focus();
+        } else {
+          createWindow();
+        }
+      }
+    },
+    {
+      label: 'Exit',
+      click: () => {
+        app.exit();
+      }
+    }
+  ]);
+
+  tray.setToolTip('Music Playlist App');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', () => {
+    if (mainWindow) {
+      mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+    } else {
+      createWindow();
+    }
+  });
+}
+
+app.whenReady().then(() => {
+  createTray();
+
+  app.on('activate', () => {
+    if (!mainWindow) createWindow();
+  });
+});
+
+app.on('window-all-closed', (e) => {
+  e.preventDefault();
+});
